2116bd1b506c3bf85344ebfdee648f55317f3396
Yash Vaidya <bobbynaldocr7@gmail.com>
2024-08-26 20:05:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 function mincost(arr)
 { 
 //write your code here
+	arr.sort((a, b) => a-b);
+	let totalCost = 0;
+
+	while(arr.length > 1) {
+		const firstRope = arr.shift();
+		const secondRope = arr.shift();
+
+		const combine = firstRope + secondRope;
+		totalCost += combine;
+	}
+	
 // return the min cost
+	return totalCost;
   
 }
 
